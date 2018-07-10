9f8c0040a8f63130ee768c0c1ece29f4f939192a
Yadiel <ycabr035@fiu.edu>
2018-07-10 17:34:38 -0400

    added functions

--- functions/coinFlipsFunc.py
+++ functions/coinFlipsFunc.py
@@ -1,10 +1,10 @@
 “””” This a simple function that simulates the toss of a coinself.
      The initial number of trials is 100 but you can call this function
-     with the number of trials you desire. 
+     with the number of trials you desire.
 “””
 import random as rd
 
-def simulate_coin_flips(num_trials):
+def coin_flips(num_trials):
     heads = 0
     tails = 0
     p_heads = 0.5
@@ -17,5 +17,5 @@ def simulate_coin_flips(num_trials):
     percent_heads = heads / num_trials
     return percent_heads
 
-percentage = simulate_coin_flips(200) # calling the function
+percentage = coin_flips(200) # calling the function and storing the result on variable
 print(percentage)
