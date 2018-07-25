b6222c687226bcbeeef108faf38c6c028599c4a0
Yadiel <ycabr035@fiu.edu>
2018-07-25 17:28:07 -0400

    added new function

--- functions/probabilityDensity.py
+++ functions/probabilityDensity.py
@@ -1,3 +1,4 @@
-# This is is a function that calculates the probability density function 
+# This is is a function that calculates the probability density function
+# Here's a link to the equation https://en.wikipedia.org/wiki/Probability_density_function
 def gaussian_density(x, mu, sigma):
     return (1/np.sqrt(2*np.pi*np.power(sigma, 2.))) * np.exp(-np.power(x - mu, 2.) / (2 * np.power(sigma, 2.)))
