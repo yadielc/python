e0d22c0f1252802e408b6e1b70aa25ac35abf3f8
Yadiel <ycabr035@fiu.edu>
2018-08-02 17:00:18 -0400

    added kalman filter

--- functions/kalmanFilters/kalman.py
+++ functions/kalmanFilters/kalman.py
@@ -5,7 +5,7 @@ def update(mean1, variance1, mean2, variance2):
     new_variance = 1./(1./variance1 + 1./variance2)
     return [new_mean, new_variance]
 
-def predict(mean1, var1, mean2, var2):
+def predict(mean1, variance1, mean2, variance2):
     new_mean = mean1 + mean2
     new_var = var1 + var2
     return [new_mean, new_var]
