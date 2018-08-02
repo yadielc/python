92f9228bd41b50f07eccf457a769d40702dad3ef
Yadiel <ycabr035@fiu.edu>
2018-08-02 16:44:59 -0400

    added update function

--- functions/kalmanFilters/update.py
+++ functions/kalmanFilters/update.py
@@ -5,3 +5,5 @@ def update(mean1, variance1, mean2, variance2):
     new_mean = (variance2 * mean1 + variance1 * mean2)/ (var1 + var2)
     new_variance = 1/ (1/var1 + 1/ var2)
     return [new_mean, new_variance]
+
+print update(5., 7., 14., 2.)
