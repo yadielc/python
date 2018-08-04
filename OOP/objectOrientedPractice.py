1de7ace8c3185f082d9de4e6c565f175352fa136
Yadiel <ycabr035@fiu.edu>
2018-08-04 16:21:59 -0400

    added practice

--- OOP/objectOrientedPractice.py
+++ OOP/objectOrientedPractice.py
@@ -2,6 +2,10 @@ import numpy as np
 import car
 
 '''
+objectOrientedPractice.property
+
+This is a review of object oriented programming in Python. It creates a car object, importing
+the information of the object from a file class called Car. 
 
 '''
 
